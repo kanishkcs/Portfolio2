bb05e65bec0313e9bf5a8c6f796b25016ee08dc4
kanishkcs <kanishkkashyap840@gmail.com>
2021-05-02 03:35:41 +0530

    7

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-(function() {
-    var words = ["Designer", "Developer", "Gamer", "Programmer"],
-        i = 0;
-    setInterval(function() {
-        $("#changingword").slideUp(function() {
-            $(this)
-                .html(words[(i = (i + 1) % words.length)])
-                .slideDown();
-        });
-    }, 3000);
-})();
+// (function() {
+//     var words = ["Designer", "Developer", "Gamer", "Programmer"],
+//         i = 0;
+//     setInterval(function() {
+//         $("#changingword").slideUp(function() {
+//             $(this)
+//                 .html(words[(i = (i + 1) % words.length)])
+//                 .slideDown();
+//         });
+//     }, 3000);
+// })();
